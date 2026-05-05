1e98de1d2ffa2b9eff7d65bbc1e205eea9332d5f
humbert9933-ux <humbert9933@gmail.com>
2026-05-04 21:04:17 -0400

    Update service worker cache name and logic

--- sw.js
+++ sw.js
@@ -1,21 +1,31 @@
-const CACHE_NAME = 'global-tyre-v1';
+const CACHE_NAME = 'globaltyre-v2.1';
 const assets = [
-  '/',
-  '/index.html',
-  '/logo.jpg',
-  '/llantas_maxell_camion.json'
+  './',
+  './index.html',
+  './logo.jpg',
+  './manifest.json',
+  './llantas_maxell_camion.json'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
     })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
   );
 });
